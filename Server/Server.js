9228a8e1757ff2c5db88461673ba3c18a90d8297
Tim Bartsch <tim-bartsch@outlook.com>
2024-06-27 13:37:18 +0200

    Zum Commit vorgemerkte Änderungen: 	geändert:       Server.js 	geändert:       ../package-lock.json 	geändert:       ../package.json

--- Server/Server.js
+++ Server/Server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const shell = require('shelljs');
 const ytdl = require('ytdl-core');
+const ping = require('ping');
 const app = express();
 const PORT = 4000;
 
+var datestat = "14:00:00";
+var activated = "False";
+
 function date() {
 	let uhr = new Date().toLocaleTimeString();
-	if(uhr=="22:00:00"){
-		shell.exec(`sudo uhubctl -l 1-1 -a off 1>&-`);
-        shell.exec(`echo Usb aus`);
+	if(datestat<="22:00:00"){
+		if(uhr>="22:00:00"){
+			if(activated=="False"){
+				shell.exec(`sudo uhubctl -l 1-1 -a off 1>&-`);
+        		shell.exec(`echo Usb aus`);
+        		activated = "True";
+			}
+		}
 	}
 	setTimeout(date, 500);
 }
@@ -28,10 +37,14 @@ app.get('/usb', async (req, res, next) => {
 		if(status=="on"){
 			shell.exec("sudo uhubctl -l 1-1 -a on 1>&-");
 			shell.exec(`echo "Usb an"`);
+			datestat = new Date().toLocalTimeString();
+			activated = "False";			
 			return res.sendStatus(200);
 		}else if(status=="off"){
 			shell.exec("sudo uhubctl -l 1-1 -a off 1>&-");
 			shell.exec(`echo Usb aus`);
+			datestat = new Date().toLocaleTimeString();
+			activated = "True";
 			return res.sendStatus(200);
 		}
 	}catch{
@@ -49,11 +62,25 @@ app.get('/start', async (req, res, next) => {
 	}
   });
 
+app.get('/testin', async (req, res, next) => {
+	var status = req.query.stat
+	try{
+		if(status=="self"){
+			return res.sendStatus(200);
+		}else{
+			let host = await ping.promise.probe(status);
+			console.log(host);
+		}
+	}catch{
+		return res.sendStatus(400);
+	}
+});
+
 app.get('/test', async (req, res, next) => {
 	return res.sendStatus(200);
 });
 
-app.get('/downloadmp3', async (req, res, next) => {
+/*app.get('/downloadmp3', async (req, res, next) => {
 	try {
 		var url = req.query.url;
 		if(!ytdl.validateURL(url)) {
@@ -105,3 +132,4 @@ app.get('/downloadmp4', async (req, res, next) => {
 		console.error(err);
 	}
 });
+*/
